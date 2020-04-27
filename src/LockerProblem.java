3943cb74374d99cea0cab22a7ae9acefefde9a03
tchekkol1 <tchekol1@umbc.edu>
2020-04-27 14:56:25 -0400

    seconde commit

--- src/LockerProblem.java
+++ src/LockerProblem.java
@@ -1,11 +1,11 @@
 public class LockerProblem {
     public static void main(String[] args) {
         boolean []locker= new boolean[100];
-        for (int i = 1; i <100 ; i++) {
+        for (int i = 0; i <locker.length ; i++) {
             locker[i]=false;
         }
-        for (int i = 1; i < 100; i++) {
-            for (int j = i; j < 100; j+=i) {
+        for (int i = 1; i <= locker.length; i++) {
+            for (int j = (i-1); j < locker.length; j+=i) {
                  if(locker[j]==true){
                     locker[j]=false;}
 else if(locker[j]==false)
@@ -13,10 +13,10 @@ else if(locker[j]==false)
 
             }
         }
-        for (int i = 1; i <100 ; i++) {
+        for (int i = 0; i <100 ; i++) {
             if (locker[i] == true) {
 
-                System.out.println("The locker that is open "+ i);
+                System.out.println("The locker that is open "+ (i+1));
             }
         }
 
